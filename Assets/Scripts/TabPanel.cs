1ebd2c425835b5ed99a1ff8d153ab19268f76530
imHyejinPark <qkrgpwls1010@gmail.com>
2021-02-02 16:04:18 +0900

    init
    초기화

---
+++ Assets/Scripts/TabPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabPanel : MonoBehaviour
+{
+    public List<TabButton> tabButtons;
+    public List<GameObject> contentsPanels;
+
+    public void ClickTab(int id)
+    {
+        for (int i = 0; i < contentsPanels.Count; i++)
+        {
+            if (i == id)
+            {
+                contentsPanels[i].SetActive(true);
+            }
+            else
+            {
+                contentsPanels[i].SetActive(false);
+            }
+        }
+    }
+}
