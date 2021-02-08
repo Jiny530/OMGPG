9b59e30b0a0a33ecf6a9fa8b1226f110d3f1f5d2
hikimrin <70943835+hikimrin@users.noreply.github.com>
2021-02-08 11:32:02 +0900

    connect

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+
+    string [] song_list = {"Birthday", "DDU-DU-DDU-DU", "Shape Of you"};
+
+    public static int selected_song, user_lev, user_ex, stick, stone, frame, dic_lev, volume;
+
+    float sink;
+
+    int[] top_score;
+
+}
