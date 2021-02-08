65ce28a8fa217b22e51527b238c7f41cb2e9a59e
imHyejinPark <qkrgpwls1010@gmail.com>
2021-02-08 17:23:43 +0900

    frame design select
    편경 틀도 같이 옷 갈아입기

---
+++ Assets/Scripts/change_cloth.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Diagnostics;
+
+public class change_cloth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public GameObject[] pyeongyeong;
+    public GameObject[] frames;
+    public Material[] material;
+    Material[] mat;
+
+    void Start()
+    {   
+        if (Data.stone < 0)
+        {
+            //원본 유지
+        }
+        else //옷갈아입히기
+        {
+            for (int i=0;i<16;i++){
+                mat = pyeongyeong[i].GetComponent<MeshRenderer>().materials;
+                mat[0] = material[Data.stone];
+                pyeongyeong[i].GetComponent<MeshRenderer>().materials = mat;
+            }
+        }
+
+        if (Data.frame < 0)
+        {
+            //원본 유지
+        }
+        else //옷갈아입히기
+        {
+            for (int i=0;i<16;i++){
+                mat = frames[i].GetComponent<MeshRenderer>().materials;
+                mat[0] = material[Data.frame];
+                frames[i].GetComponent<MeshRenderer>().materials = mat;
+            }
+        }
+    }
+
+}
--- Assets/Scripts/selected_thema.cs
+++ Assets/Scripts/selected_thema.cs
@@ -9,8 +9,15 @@ public class selected_thema : MonoBehaviour
     //해당 이미지를 클릭하면 나머지는 비활성화, 선택된것만 활성화, 변수에 해당 인덱스 저장
     //선택된 이미지를 한번 더 클릭시 비활성화, 변수에 -1 저장
 
+    void start(){
+        if (Data.stone != -1 ){
+            content[Data.stone].SetActive(true); 
+            print(Data.stone);
+        }
+    }
     public void select(int index){
 
+        
         for (int i = 0; i < 5; i++){
             content[i].SetActive(false);  
         }
@@ -20,6 +27,7 @@ public class selected_thema : MonoBehaviour
         else{
             content[index].SetActive(true); 
             Data.stone = index;
+            Data.frame = index;
         }            
     }
 }
