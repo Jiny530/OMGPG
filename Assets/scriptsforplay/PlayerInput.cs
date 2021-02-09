62d63e650730063affde1cdab1646cfc3bd0ca6d
kyuwonii <45291344+kyuwonii@users.noreply.github.com>
2021-02-09 14:14:55 +0900

    controller first & obj remove

---
+++ Assets/scriptsforplay/PlayerInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    TimingManager theTimingManager;
+
+    private void Start()
+    {
+        theTimingManager = FindObjectOfType<TimingManager>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            theTimingManager.CheckTiming();
+        }
+    }
+}
