aabad3ca42d9e8d5cc3a2ff71e7274dbd0baeb91
yeawon-you <55146205+yeawon-you@users.noreply.github.com>
2021-02-16 13:42:02 +0900

    montor demo ready complete
    멘토님용 상사화 데모 완성

--- Assets/scriptsforplay/NoteManager.cs
+++ Assets/scriptsforplay/NoteManager.cs
@@ -22,12 +22,12 @@ public class NoteManager : MonoBehaviour
         
         currentTime += Time.deltaTime;//1초씩 증가
 
-        if (currentTime >= 60d / bpm)
+        if (currentTime >= 180d / bpm)
         {//다음 비트가 나올 차례가 되면
             GameObject t_note = Instantiate(goNote, tfNoteAppear.position, Quaternion.identity);//노트를 등장 시키기
             t_note.transform.SetParent(this.transform);
             theTimingManager.boxNoteList.Add(t_note);
-            currentTime -= 60d / bpm;//오차가 쬐끔 생기기 때문에... 게임은 프레임 타임이어서,... 그거 챙기려고..0으로 초기화x 쌓이면 점점 밀림
+            currentTime -= 180d / bpm;//오차가 쬐끔 생기기 때문에... 게임은 프레임 타임이어서,... 그거 챙기려고..0으로 초기화x 쌓이면 점점 밀림
         }
         
     }
