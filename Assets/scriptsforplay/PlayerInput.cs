ff0d275d185a6bbac977d43fca952f33f2bb7d56
kyuwonii <45291344+kyuwonii@users.noreply.github.com>
2021-02-19 12:33:28 +0900

    Volume

--- Assets/scriptsforplay/PlayerInput.cs
+++ Assets/scriptsforplay/PlayerInput.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerInput : MonoBehaviour
 {
@@ -16,6 +17,10 @@ public class PlayerInput : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             theTimingManager.CheckTiming();
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.Touch))
+        {
+            SceneManager.LoadScene("Main");
         }
     }
 }
