0d58de0fbe7d817a66e46ab7803ca494faa6a901
yeawon-you <55146205+yeawon-you@users.noreply.github.com>
2021-02-23 17:14:15 +0900

    선택된 곡에 맞게 변수 적용하도록(bpm 등) 세팅

--- Assets/scriptsforplay/Note.cs
+++ Assets/scriptsforplay/Note.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class Note : MonoBehaviour
 {
     public float noteSpeed = 400;
-    //public static string[] strAns = { "pg7", "pg14", "pg12", "pg14", "pg12", "pg14", "pg12", "pg14", "pg7", "pg14", "pg12", "pg14", "pg7", "pg5", "pg7" };//상사화
-    public static int[] tmpAns = { 7, 14, 12, 14, 12, 14, 12, 14, 7, 14, 12, 14, 7, 5, 7 };//상사화
+    public static int[] ansPlay = Data.answers[Data.selected_song];
     public int ans; 
     UnityEngine.UI.Image noteImage;
 
     private void Start()
     {
-        ans = tmpAns[NoteManager.noteCnt-1];
+        ans = ansPlay[NoteManager.noteCnt-1];
         noteImage = GetComponent<UnityEngine.UI.Image>();
     }
+
     public void HideNote()
     {
         noteImage.enabled = false;
     }
-    // Update is called once per frame
+
     void Update()
     {
         transform.localPosition += Vector3.right * noteSpeed * Time.deltaTime;//꼭 로컬 하기
