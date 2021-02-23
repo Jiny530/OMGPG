b0c5580bdf8c26534881fa82a20b56ae3e8a97bd
kyuwonii <45291344+kyuwonii@users.noreply.github.com>
2021-02-23 13:51:10 +0900

    자유연주모드 + sync 씬
    특경 넣었음

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -13,12 +13,12 @@ public class Data : MonoBehaviour
             new[] {7, 14, 12, 14, 12, 14, 12, 14, 7, 14, 12, 14, 7, 5, 7},//상사화
             new[] { 1, 2 }//해리포터
         };
-
     public static int selected_song=-1, user_lev, user_ex, dic_lev, volume;
     public static int stone = -1;
     public static int frame = -1;
     public static int stick = -1;
     public static double usersyncDelay;
+
     int[] top_score= { 0, 0, 0 };
 
 }
