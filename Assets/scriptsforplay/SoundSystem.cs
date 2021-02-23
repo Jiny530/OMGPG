206f1721d324975545ff8cde7944a8e079c3c09d
yeawon-you <55146205+yeawon-you@users.noreply.github.com>
2021-02-23 17:14:15 +0900

    선택된 곡에 맞게 변수 적용하도록(bpm 등) 세팅

---
+++ Assets/scriptsforplay/SoundSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSystem : MonoBehaviour
+{
+    public static AudioSource[] audios;
+
+    void Start()
+    {
+        audios = GameObject.Find("SoundSystem").GetComponents<AudioSource>();
+        //여기에 음악 경로로 다 써놓기 Find 하지 않기 위해.
+    }
+}
