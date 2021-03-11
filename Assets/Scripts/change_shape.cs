b2632fed9ba166643df89863245c43174ca6561b
imHyejinPark <53827279+imHyejinPark@users.noreply.github.com>
2021-03-11 19:59:31 +0900

    각퇴 모양변경
    각퇴 옷입히기~

---
+++ Assets/Scripts/change_shape.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class change_shape : MonoBehaviour
+{
+    public GameObject gaktoe;
+    public Mesh[] meshs;
+    private MeshFilter meshFilter;
+    public Material[] material;
+    Material[] mat;
+
+    void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        meshFilter.sharedMesh = meshs[Data.stick];
+
+        mat = gaktoe.GetComponent<MeshRenderer>().materials;
+        mat[0] = material[Data.stick];
+        gaktoe.GetComponent<MeshRenderer>().materials = mat;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
