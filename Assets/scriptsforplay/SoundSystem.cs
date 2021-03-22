f6019ebe1784026e18befef2045a789ec74d7de6
kyuwonii <hikimrin@ewhain.net>
2021-03-22 14:38:26 +0900

    Sound

--- Assets/scriptsforplay/SoundSystem.cs
+++ Assets/scriptsforplay/SoundSystem.cs
@@ -7,15 +7,17 @@ public class SoundSystem : MonoBehaviour
     [SerializeField] AudioSource[] bgms = null;
     [SerializeField] AudioSource[] pgMusics = null;
 
-    public AudioSource pgMusic;
-    public AudioSource bgm;
+    public static AudioSource pgMusic;
+    public static AudioSource bgm;
 
     void Start()
     {
         pgMusic=pgMusics[Data.selected_song];
         bgm=bgms[Data.selected_song];
-        pgMusics[Data.selected_song].Play(0);
-        bgms[Data.selected_song].Play(0);
+        pgMusic.volume = Data.volumes[0];
+        pgMusic.Play(0);
+        bgm.volume = Data.volumes[1];
+        bgm.Play(0);
     }
 
     void Update(){
