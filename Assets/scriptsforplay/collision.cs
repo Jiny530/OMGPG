999b3d35f62bd0abcda148eec318f89b4fa43bef
kyuwonii <hikimrin@ewhain.net>
2021-04-05 17:39:24 +0900

    자유연주모드 타격세기 적용
    타격세기에 따른 볼륨 & 진동조절 + 음계 온오프 적용

--- Assets/scriptsforplay/collision.cs
+++ Assets/scriptsforplay/collision.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using System.Diagnostics;
 
 public class collision : MonoBehaviour
 {
@@ -17,12 +16,15 @@ public class collision : MonoBehaviour
     public float m_countToStop;
     public double hit_duration=1f;//따닥 방지변수
 
+    private Vector3 oldPosition;
+    private Vector3 currentPosition;
+    private double velocity;
     void Start()
     {
         for (int i=0; i<pg_sound.Length; i++){
             pg_sound[i].volume = Data.volumes[0];
         }
-       // snare_compo = snare_obj.GetComponent<snare>();
+        oldPosition = transform.position;
     }
 
     void Update()
@@ -36,35 +38,51 @@ public class collision : MonoBehaviour
             }
         }
         hit_duration += Time.deltaTime;
+
+        if (Data.selected_song == 0){
+            currentPosition = transform.position;
+            var dis = (currentPosition - oldPosition);
+            var distance = Math.Sqrt(Math.Pow(dis.x,2)+Math.Pow(dis.y,2)+Math.Pow(dis.z,2));
+            velocity = distance / Time.deltaTime;
+            oldPosition = currentPosition;
+            
+        }
+        
     }
 
     public void OnCollisionEnter(Collision col)
     {
-        if (hit_duration < 0.5)//따닥 방지.
+        if (hit_duration < 0.3)//따닥 방지
             return;
-        Vibrate(50);
+
         //OVRInput.SetControllerVibration(0.3f, 0.3f, OVRInput.Controller.RTouch);
 
         for (int i = 0; i < 16; i++)
         {
             if (col.collider.CompareTag(tags[i]))
             {
-                pg_sound[i].PlayOneShot(pg_sound[i].clip);
-                hitCheck = i;
-                if (snares[i].activeSelf)
-                { //collision으로는 자식객체 접근 불가능해서 그냥 snare로 접근
-                    snares[i].SetActive(false);
+                if (Data.selected_song != 0){ //리듬게임 모드
+                    pg_sound[i].PlayOneShot(pg_sound[i].clip);
+                    Vibrate(50f);
+                    if (snares[i].activeSelf) snares[i].SetActive(false);
                 }
+                else{ //자유연주모드
+                    Debug.Log(velocity);
+                    pg_sound[i].volume = Data.volumes[0] * (float)velocity;
+                    pg_sound[i].PlayOneShot(pg_sound[i].clip);
+                    Vibrate(50* (float)velocity);
+                }
+                hitCheck = i;
                 break;
             }
         }
         hit_duration = 0f;//0.5초간 다시 충돌 함수가 호출되지 않도록 구현,, 너무 길다 싶으면 줄일 수도 있구여@ 한 프레임은 0.2초 정도 하는 것 같음.
     }
 
-    void Vibrate(int ms)
+    void Vibrate(float ms)
     {
         OVRInput.SetControllerVibration(0.4f, 0.4f, OVRInput.Controller.RTouch);
-        m_countToStop = ((float)ms) / 1000;
+        m_countToStop = (ms) / 1000;
     }
 
     void StopVibration()
